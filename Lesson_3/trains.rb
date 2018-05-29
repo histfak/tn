9608214c626caff30c1444a64213df75a342bdb8
histfak@gmail.com <histfak@gmail.com>
2018-05-29 23:01:35 +0300

    Various fixes by curators comments and suggestions

--- Lesson_3/trains.rb
+++ Lesson_3/trains.rb
@@ -1,82 +1,78 @@
 #!/usr/bin/env ruby
 class Station
-  
   attr_reader :name, :trains
-  
+
   def initialize(name)
     @name = name
     @trains = []
   end
-  
+
   def train_arrival(train)
     trains << train
   end
-  
+
   def train_departure(train)
     trains.delete(train)
     train.station = nil
   end
-  
+
   def trains_status(type = nil)
     if type.nil?
-      puts 'Trains: '
-      trains.each { |train| puts train.number }
+      trains
     else
-      temp = []
-      trains.each { |train| temp << train.number if train.type == type }
-      if temp.empty?
-        puts 'Not found!'
-      else
-        puts "Trains(#{type}): "
-        temp.each { |x| puts x }
-      end
+      trains.select { |train| type == train.type }
     end
   end
-  
 end
 
 class Route
-  
-  attr_accessor :stations, :first, :terminal
-  
-  def initialize (first, terminal)
+  attr_reader :stations
+
+  def initialize(first, terminal)
     @stations = [first, terminal]
   end
 
   def add_station(station)
-    self.stations.insert(-2, station) 
+    self.stations.insert(-2, station)
   end
 
   def remove_station(station)
     if [stations.first, stations.last].include?(station)
-      raise 'You can\'t remove the first or terminal station!'
-    else 
+      puts 'You can\'t remove the first or terminal station!'
+    else
       self.stations.delete(station)
     end
   end
 
   def list_stations
     puts 'The route contains the following stations: '
-    stations.each{ |station| puts station.name }
+    stations.each { |station| puts station.name }
   end
-    
 end
 
 class Train
-
-  attr_accessor :speed, :route, :station, :quantity
+  attr_accessor :speed, :route, :station, :cars
   attr_reader :number, :type
 
-  def initialize(number, type, quantity)
+  def initialize(number, type, cars)
     @number = number
     @type = type
-    @quantity = quantity
+    @cars = cars
     @speed = 0
   end
-  
+
   def set_route(route)
     self.route = route
     self.station = route.stations.first
+    route.stations.first.train_arrival(self)
+  end
+
+  def set_speed(speed)
+    if speed > 0
+      self.speed = speed
+    else
+      puts 'Speed must be > 0!'
+    end
   end
 
   def stop
@@ -84,50 +80,67 @@ class Train
   end
 
   def add_carriage
-    if speed.zero? 
-      self.quantity += 1
-    else 
-      raise 'You can\'t add the carriage in motion!'
+    if speed.zero?
+      self.cars += 1
+    else
+      puts 'You can\'t add the carriage in motion!'
     end
   end
 
   def remove_carriage
-    raise "The train has no carriages!" if quantity.zero?
-    if speed.zero? 
-      self.quantity -= 1
-    else 
-      raise 'You can\'t remove the carriage in motion!'
+    puts "The train has no carriages!" if cars.zero?
+    if speed.zero?
+      self.cars -= 1
+    else
+      puts 'You can\'t remove the carriage in motion!'
     end
   end
 
-  def list_adjacent_stations
+  def list_current_station
     if route.nil?
-      raise 'You have to add the route first!'
+      puts 'You have to add the route first!'
     else
-      station_index = route.stations.index(station)
-      puts "The current station is #{station.name}."
-      puts "The previous station is #{route.stations[station_index - 1].name}." if station_index != 0
-      puts "The next station is #{route.stations[station_index + 1].name}." if station_index != route.stations.size - 1
+      station.name
     end
   end
 
-  def move(destination)
+  def list_previous_station
     if route.nil?
-      raise 'You have to add the route first!'
-    elsif destination == station
-      raise "The train \##{@number} stands at the same station (#{self.station.name})!"
-    elsif route.stations.include?(destination)
-      station_index = route.stations.index(station)
-      if (destination == route.stations[station_index - 1] && station_index != 0) || (destination == route.stations[station_index + 1] && station_index != route.stations.size - 1)
-        station.train_departure(self) if station
-        self.station = destination
-        station.train_arrival(self)
-      else
-        raise 'You have to choose the nearest station!'
-      end
+      puts 'You have to add the route first!'
     else
-      raise "The route doesn\'t have the #{station.name} station"
+      station_index = route.stations.index(station)
+      route.stations[station_index - 1].name if station_index != 0
     end
   end
-  
+
+  def list_next_station
+    if route.nil?
+      puts 'You have to add the route first!'
+    else
+      station_index = route.stations.index(station)
+      route.stations[station_index + 1].name if station_index != route.stations.size - 1
+    end
+  end
+
+  def move_forward
+    if route.nil?
+      puts 'You have to add the route first!'
+    else
+      station_index = route.stations.index(station)
+      station.train_departure(self)
+      self.station = route.stations[station_index.next]
+      station.train_arrival(self)
+    end
+  end
+
+  def move_backward
+    if route.nil?
+      puts 'You have to add the route first!'
+    else
+      station_index = route.stations.index(station)
+      station.train_departure(self)
+      self.station = route.stations[station_index.pred]
+      station.train_arrival(self)
+    end
+  end
 end
