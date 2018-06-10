96bdf7ff099659317f841737c999e016d32dfa5a
histfak@gmail.com <histfak@gmail.com>
2018-06-11 01:04:45 +0300

    type select fix

--- Lesson_6/text_ui.rb
+++ Lesson_6/text_ui.rb
@@ -160,6 +160,7 @@ class TextUi
       @trains << PassengerTrain.new(number)
     elsif type == 'Cargo'
       @trains << CargoTrain.new(number)
+    else raise 'Wrong train type!'
     end
     puts 'The train has just been created.'
   rescue RuntimeError => e
