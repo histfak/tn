764351e002a6b82d2c35ea8f7d843e3fad3cda8a
histfak@gmail.com <histfak@gmail.com>
2018-06-19 11:20:17 +0300

    Lesson #9 accessors fix

--- Lesson_9/accessors.rb
+++ Lesson_9/accessors.rb
@@ -1,23 +1,21 @@
 module Accessors
   def attr_accessor_with_history(*args)
     args.each do |name|
-      var_name = "@#{name}".to_sym
-      history = []
-      define_method(name) { instance_variable_get(var_name) }
+      define_method(name) { instance_variable_get("@#{name}".to_sym) }
       define_method("#{name}_history".to_sym) { instance_variable_get("@#{name}_history".to_sym) }
       define_method("#{name}=".to_sym) do |value|
-        instance_variable_set(var_name, value)
-        instance_variable_set("@#{name}_history".to_sym, history << value)
+        history = (instance_variable_get("@#{name}_history") || [])
+        instance_variable_set("@#{name}_history".to_sym, history << instance_variable_get("@#{name}".to_sym))
+        instance_variable_set("@#{name}".to_sym, value)
       end
     end
   end
 
   def strong_attr_accessor(name, class_name)
-    var_name = "@#{name}".to_sym
-    define_method(name) { instance_variable_get(var_name) }
+    define_method(name) { instance_variable_get("@#{name}".to_sym) }
     define_method("#{name}=".to_sym) do |value|
       raise 'Wrong class!' if class_name != value.class
-      instance_variable_set(var_name, value)
+      instance_variable_set("@#{name}".to_sym, value)
     end
   end
 end
